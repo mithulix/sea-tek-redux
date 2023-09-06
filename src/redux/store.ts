e078e97c079b2c2324585641f175bd5b11e704d3
mithulix <mohaiminul.devs@gmail.com>
2023-09-06 11:40:16 +0600

    sea tek redux, react-redux, typescript, radix-ui, vite, tailwindcss

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    product: productReducer
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
